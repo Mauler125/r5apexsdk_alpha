00c046f01ad726c226e343e10054b1f58c91ef2f
Amos <48657826+Mauler125@users.noreply.github.com>
2021-11-16 01:21:27 +0100

    Update CMatSystemSurface.h

--- r5dev/CMatSystemSurface.h
+++ r5dev/CMatSystemSurface.h
@@ -5,5 +5,5 @@ namespace
 {
 	/* ==== CMATSYSTEMSURFACE =============================================================================================================================================== */
 	ADDRESS p_CMatSystemSurface_DrawColoredText = g_mGameDll.FindPatternSIMD((std::uint8_t*)"\x4C\x8B\xDC\x48\x83\xEC\x68\x49\x8D\x43\x58\x0F\x57\xC0", "xxxxxxxxxxxxxx"); /*4C 8B DC 48 83 EC 68 49 8D 43 58 0F 57 C0*/
-	void* (*CMatSystemSurface_DrawColoredText)(void* thisptr, int a0, int fontHeight, int offsetX, int offsetY, int red, int green, int blue, int alpha, const char* text, ...) = (void* (*)(void*, int, int, int, int, int, int, int, int, const char*, ...))p_CMatSystemSurface_DrawColoredText.GetPtr();
+	void* (*CMatSystemSurface_DrawColoredText)(void* thisptr, int font, int fontHeight, int offsetX, int offsetY, int red, int green, int blue, int alpha, const char* text, ...) = (void* (*)(void*, int, int, int, int, int, int, int, int, const char*, ...))p_CMatSystemSurface_DrawColoredText.GetPtr();
 }
