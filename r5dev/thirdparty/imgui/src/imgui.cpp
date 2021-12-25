dad2c38fe0fdc09b3eec9eb083165c389f285366
Amos <48657826+Mauler125@users.noreply.github.com>
2021-12-25 17:26:09 +0100

    Change ImGui default paths

--- r5dev/thirdparty/imgui/src/imgui.cpp
+++ r5dev/thirdparty/imgui/src/imgui.cpp
@@ -1090,8 +1090,8 @@ ImGuiIO::ImGuiIO()
     DisplaySize = ImVec2(-1.0f, -1.0f);
     DeltaTime = 1.0f / 60.0f;
     IniSavingRate = 5.0f;
-    IniFilename = "imgui.ini"; // Important: "imgui.ini" is relative to current working dir, most apps will want to lock this to an absolute path (e.g. same path as executables).
-    LogFilename = "imgui_log.txt";
+    IniFilename = "platform\\layout.ini"; // Important: "imgui.ini" is relative to current working dir, most apps will want to lock this to an absolute path (e.g. same path as executables).
+    LogFilename = "platform\\logs\\imgui_log.txt";
     MouseDoubleClickTime = 0.30f;
     MouseDoubleClickMaxDist = 6.0f;
     for (int i = 0; i < ImGuiKey_COUNT; i++)
